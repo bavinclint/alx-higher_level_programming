7b969a6eade4352e364097df6b20616b1e9bd034
bavinclint <bavinclint.com>
2022-08-22 23:19:38 +0300

    Linked list cycle

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,28 +1,43 @@
 #include "lists.h"
 
-
 /**
- * check_cycle - checks the linked list if it is a cycle
- * @list: pointer to a list
- *
- * Return: 1 if cucle or 0
+ * check_cycle - checks if a singly linked list has
+ * a cycle in it
+ * @list: pointer to the list
+ * Return: 0 if there is no cycle,
+ * 1 if there is a cycle
  */
-
 int check_cycle(listint_t *list)
 {
-	listint_t *step;
-	listint_t *step2;
+	listint_t *p2;
+	listint_t *prev;
 
-	if (list == NULL)
-		return (0);
-	step = list;
-	step2 = list;
-	while (step2 != NULL && step2->next != NULL)
+	p2 = list;
+	prev = list;
+	while (list && p2 && p2->next)
 	{
-		step = step->next;
-		step2 = step2->next->next;
-		if (step == step2)
+		list = list->next;
+		p2 = p2->next->next;
+
+		if (list == p2)
+		{
+			list = prev;
+			prev =  p2;
+			while (1)
+			{
+				p2 = prev;
+				while (p2->next != list && p2->next != prev)
+				{
+					p2 = p2->next;
+				}
+				if (p2->next == list)
+					break;
+
+				list = list->next;
+			}
 			return (1);
+		}
 	}
+
 	return (0);
 }
