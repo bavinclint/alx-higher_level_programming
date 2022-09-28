2264f88306cc56403ed64e62bfe9c6efa4147123
bavinclint <bavinclint@gmail.com>
2022-09-28 05:48:08 +0300

    Student to JSON with filter

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,24 +1,15 @@
 #!/usr/bin/python3
-"""Contains the clas "Student"""
+"""student class"""
 
 
 class Student:
-    """Representation of a student"""
+    """class stuff"""
     def __init__(self, first_name, last_name, age):
-        """Initializes the student"""
+        """initializes"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
-    def to_json(self, attrs=None):
-        """returns a dictionary representation of a Student instance
-        with specified attributes"""
-        if attrs is None:
-            return self.__dict__
-        new_dict = {}
-        for a in attrs:
-            try:
-                new_dict[a] = self.__dict__[a]
-            except:
-                pass
-        return new_dict
+    def to_json(self):
+        """prints __dict__"""
+        return self.__dict__
