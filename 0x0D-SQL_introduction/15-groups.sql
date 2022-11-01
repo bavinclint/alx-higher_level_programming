583f621c2964c1f8ab4047607cb75402390ea1ce
bavinclint <bavinclint@gmail.com>
2022-11-01 22:06:18 +0300

    Say my name

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,3 +1,2 @@
--- counts and shows
-SELECT score, COUNT(score) AS number FROM second_table
-GROUP BY score DESC;
+-- lists the number of records with the same score in the table second_table
+SELECT score, COUNT(1) AS number FROM second_table GROUP BY score ORDER BY score DESC;
