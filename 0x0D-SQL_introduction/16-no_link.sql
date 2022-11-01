fefd4e220ff83fb088a68cdd3c77925595ede001
bavinclint <bavinclint@gmail.com>
2022-11-01 22:08:26 +0300

    Say my name

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,4 +1,2 @@
--- no name order
-SELECT score, name FROM second_table
-WHERE name <> ''
-GROUP BY score DESC, name DESC;
+-- lists all records of the table second_table
+SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
