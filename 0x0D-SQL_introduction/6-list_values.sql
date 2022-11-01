f21d1ea3b7af93c40037d0e350685eff36eed376
bavinclint <bavinclint@gmail.com>
2022-11-01 21:54:21 +0300

    First add

--- 0x0D-SQL_introduction/6-list_values.sql
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -1,5 +1,2 @@
--- creates database and table
-CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-USE hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS states
-(`id` INT AUTO_INCREMENT, `name` VARCHAR(256) NOT NULL, PRIMARY KEY(id));
+-- lists all rows of the table first_table from the database
+SELECT * FROM first_table;
