39b7b084a934a5442d0b751e2a0d186d3d6494f1
bavinclint <bavinclint@gmail.com>
2022-11-02 23:10:52 +0300

    Only Comedy

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,2 +1,4 @@
--- script that lists all shows contained in hbtn_0d_tvshows
-SELECT tv_genres.name AS genre, count(*) AS number_shows FROM tv_genres LEFT JOIN tv_show_genres ON tv_show_genres.genre_id=tv_genres.id WHERE tv_show_genres.show_id IS NOT NULL GROUP BY genre ORDER BY number_shows DESC;
+-- lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each.
+SELECT tv_genres.name AS genre, COUNT(1) AS number_shows
+FROM tv_genres JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY genre ORDER BY number_shows DESC;
