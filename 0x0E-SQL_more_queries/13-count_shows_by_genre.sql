5b9fc385248511895010c88ce876585a00b7942d
bavinclint <bavinclint@gmail.com>
2022-11-02 23:07:10 +0300

    My genres

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,9 +1,2 @@
--- genre id by show
-SELECT tgs.name AS genre, COUNT(tg.genre_id) AS number_shows
-FROM tv_shows AS ts
-LEFT JOIN tv_show_genres AS tg
-ON ts.id = tg.show_id
-LEFT JOIN tv_genres AS tgs
-ON tg.genre_id = tgs.id
-WHERE tg.genre_id IS NOT NULL
-GROUP BY tgs.name ORDER BY number_shows DESC;
+-- script that lists all shows contained in hbtn_0d_tvshows
+SELECT tv_genres.name AS genre, count(*) AS number_shows FROM tv_genres LEFT JOIN tv_show_genres ON tv_show_genres.genre_id=tv_genres.id WHERE tv_show_genres.show_id IS NOT NULL GROUP BY genre ORDER BY number_shows DESC;
