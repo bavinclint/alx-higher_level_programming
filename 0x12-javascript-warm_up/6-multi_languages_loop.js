9ef9acc7b395d84a42e6d7d59ed11c98cbe1911a
bavinclint <bavinclint@gmail.com>
2022-11-28 22:41:59 +0300

    Loop to languages

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-let myArr = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const myArr = ['C is fun', 'Python is cool', 'Javascript is amazing'];
 for (let i = 0; i < myArr.length; ++i) {
   console.log(myArr[i]);
 }
