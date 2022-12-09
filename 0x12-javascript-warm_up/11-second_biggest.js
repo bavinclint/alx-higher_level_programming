efd2da8e3ad74132af54e08ca6b396b9b2c3329c
bavinclint <bavinclint@gmail.com>
2022-12-09 21:00:26 +0300

    script to replace the value

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,10 +1,29 @@
 #!/usr/bin/node
 
-function secondMax (x) {
-  return x.sort().reverse()[1];
-}
-if (process.argv.length < 4) {
+let second = parseInt(process.argv[3]);
+
+if (isNaN(second)) {
   console.log('0');
-} else {
-  console.log(secondMax(process.argv.slice(2)));
+  process.exit();
 }
+
+let first = parseInt(process.argv[2]);
+
+if (first < second) {
+  const temp = first;
+  first = second;
+  second = temp;
+}
+
+for (let i = 4; i < process.argv.length; i++) {
+  const next = parseInt(process.argv[i]);
+  if (next > second) {
+    if (next > first) {
+      second = first;
+      first = next;
+    } else {
+      second = next;
+    }
+  }
+}
+console.log(second);
