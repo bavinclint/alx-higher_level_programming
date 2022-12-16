90739788b8a6984c3ce193d8b4e4f144008d7edd
bavinclint <bavinclint@gmail.com>
2022-12-16 16:52:14 +0300

    testing cases

--- 0-hbtn_status.py/0-hbtn_status.py
+++ 0-hbtn_status.py/0-hbtn_status.py
@@ -1,18 +1,17 @@
 #!/usr/bin/python3
 """
-    script that fetches https://intranet.hbtn.io/status
+fetches URL with urllib
 """
 
 
-from urllib import request
+import urllib.request
 
 
 if __name__ == "__main__":
-    with request.urlopen('https://intranet.hbtn.io/status') as response:
-        content = response.read()
-        charsetStatus = content.decode("utf-8")
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        html = response.read()
 
-        print("Body response:")
-        print("\t- type: {}".format(type(content)))
-        print("\t- content: {}".format(content))
-        print("\t- utf8 content: {}".format(charsetStatus))
+    print("Body response:")
+    print("\t- type: {}".format(type(html)))
+    print("\t- content: {}".format(html))
+    print("\t- utf8 content: {}".format(html.decode('utf-8')))
