1a72a4171a6ffcb59d037085a826c1de71242aa3
bavinclint <bavinclint@gmail.com>
2023-02-09 13:25:14 +0300

    Integers addition

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -1,50 +1,56 @@
 #!/usr/bin/python3
-"""Module that contains a function to divide a matrix by a scalar"""
+""" matrix_mul module """
 
 
-def matrix_mul(m_a, m_b):
-    """Multiplies two matrices"""
-    if type(m_a) is not list:
+def matrix_mul(prmMatrixA, prmMatrixB):
+    """ matrix_mul function
+
+    this function multiply one matrix by a second one
+
+    Attributes:
+        prmMatrixA: first matrix
+        prmMatrixB: second matrix
+    """
+    if prmMatrixA is None:
+        raise TypeError("m_a should be indicate")
+    if prmMatrixB is None:
+        raise TypeError("m_b should be indicate")
+    if not isinstance(prmMatrixA, list):
         raise TypeError("m_a must be a list")
-    if type(m_b) is not list:
+    if not isinstance(prmMatrixB, list):
         raise TypeError("m_b must be a list")
-    for x in m_a:
-        if type(x) is not list:
-            raise TypeError("m_a must be a list of lists")
-    for x in m_b:
-        if type(x) is not list:
-            raise TypeError("m_b must be a list of lists")
-    if len(m_a) < 1:
-        raise ValueError("m_a can't be empty")
-    if len(m_b) < 1:
-        raise ValueError("m_b can't be empty")
-    arowlen = len(m_a[0])
-    if arowlen < 1:
-        raise ValueError("m_a can't be empty")
-    browlen = len(m_b[0])
-    if browlen < 1:
-        raise ValueError("m_b can't be empty")
-    for row in m_a:
-        if len(row) != arowlen:
-            raise TypeError("each row of m_a must should be of the same size")
-        for col in row:
-            if type(col) is not float and type(col) is not int:
-                raise TypeError("m_a should contain only integers or floats")
-    for row in m_b:
-        if len(row) != browlen:
-            raise TypeError("each row of m_b must should be of the same size")
-        for col in row:
-            if type(col) is not float and type(col) is not int:
-                raise TypeError("m_b should contain only integers or floats")
-    if arowlen != len(m_b):
-        raise ValueError("m_a and m_b can't be multiplied")
-    newmatrix = []
-    for x in range(len(m_a)):
-        newrow = []
-        for y in range(browlen):
-            sum = 0
-            for z in range(arowlen):
-                sum += m_a[x][z] * m_b[z][y]
-            newrow.append(sum)
-        newmatrix.append(newrow)
-    return newmatrix
+    if not all(isinstance(ele, list) for ele in prmMatrixA):
+        raise TypeError("m_a must be a list of lists")
+    if not all(isinstance(ele, list) for ele in prmMatrixB):
+        raise TypeError("m_b must be a list of lists")
+    if len(prmMatrixA) == 0 or len(prmMatrixA[0]) == 0:
+        raise TypeError("m_a can't be empty")
+    if len(prmMatrixB) == 0 or len(prmMatrixB[0]) == 0:
+        raise TypeError("m_b can't be empty")
+
+    columnLenA = len(prmMatrixA[0])
+
+    for row in range(len(prmMatrixA)):
+        if len(prmMatrixA[row]) != columnLenA:
+            raise TypeError("each row of m_a must be of the same size")
+
+    columnLenB = len(prmMatrixB[0])
+
+    for row in range(len(prmMatrixB)):
+        if len(prmMatrixB[row]) != columnLenB:
+            raise TypeError("each row of m_b must be of the same size")
+
+    # initialize result matrix
+    result = [
+        [0 for x in range(len(prmMatrixA[0]))] for y in range(len(prmMatrixA))
+        ]
+
+    # iterate through rows of prmMatrixA
+    for i in range(len(prmMatrixA)):
+        # iterate through columns of prmMatrixB
+        for j in range(len(prmMatrixB[0])):
+            # iterate through rows of prmMatrixB
+            for k in range(len(prmMatrixB)):
+                result[i][j] += prmMatrixA[i][k] * prmMatrixB[k][j]
+
+    return result
