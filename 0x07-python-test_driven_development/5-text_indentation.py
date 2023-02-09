a00613397e4973803a45f21c81aa7d21c8ba54b3
bavinclint <bavinclint@gmail.com>
2023-02-09 13:27:48 +0300

    Integers addition

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,29 +1,25 @@
 #!/usr/bin/python3
-""" text_indentation module """
+"""Module containing a function that separates sentences"""
 
 
-def text_indentation(prmText):
-    """ text_indentation function
-
-    this function split a text by punctuation
-
-    Attributes:
-        prmText: text to split
-    """
-    if not isinstance(prmText, str):
+def text_indentation(text):
+    """Separates sentences in a given piece of text"""
+    if type(text) is not str:
         raise TypeError("text must be a string")
-
-    prmText = prmText.strip()
-    prmText = prmText.replace(".", ".\n")
-    prmText = prmText.replace("?", "?\n")
-    prmText = prmText.replace(":", ":\n")
-    prmText = prmText.rstrip()
-
-    if len(prmText) == 0:
-        return
-
-    for key, line in enumerate(list(prmText.split("\n"))):
-        line = line.strip()
-        print("{}".format(line))
-        if key < len(list(prmText.split("\n"))) - 1:
-            print()
+    end = 0
+    textlen = len(text)
+    while end < textlen:
+        start = end
+        while start < textlen and text[start] == ' ':
+            start += 1
+        end = start
+        while (end < textlen and text[end] != '.' and text[end] != ':' and
+               text[end] != '?'):
+            end += 1
+        if end < textlen:
+            end += 1
+        print(text[start:end], end="")
+        end -= 1
+        if text[end] == '.' or text[end] == '?' or text[end] == ':':
+            print("\n")
+        end += 1
