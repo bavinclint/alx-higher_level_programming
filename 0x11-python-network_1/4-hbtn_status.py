37563eb806e39daf83f549bf03a3647ef9dbc57e
bavinclint <bavinclint@gmail.com>
2023-03-01 22:36:14 +0300

    fetches data

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -7,7 +7,7 @@ use the package requests to fetch url
 import requests
 
 
-r = requests.get('https://intranet.hbtn.io/status')
+r = requests.get('https://alx-intranet.hbtn.io/status')
 print("Body response:")
 print("\t- type: {}".format(type(r.text)))
 print("\t- content: {}".format(r.text))
