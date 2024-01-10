db6e4d078ece39c8c30e7bac6969f0c48991f0f5
bavinclint <bavinclint@gmail.com>
2024-01-10 09:35:08 +0300

    0_add file definition

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -2,8 +2,8 @@
 """
 This is a module that imports a function add and prints the results
 """
-# if __name__ == "__main__":
-from add_0 import add
-a = 1
-b = 2
-print("{} + {} = {}".format(a, b, add(a, b)))
+if __name__ == "__main__":
+    from add_0 import add
+    a = 1
+    b = 2
+    print(f"{a} + {b} = {add(a, b)}\n")
